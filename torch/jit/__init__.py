cf5a260465112fb73d068b57c7c51e1da0edfb7f
davidriazati <davidriazati@fb.com>
2019-04-22 13:46:59 -0700

    [jit] Try to script all Python functions
    gh-metadata: pytorch pytorch 19578 gh/driazati/21/head

--- torch/jit/__init__.py
+++ torch/jit/__init__.py
@@ -706,20 +706,6 @@ def _try_get_overloaded_fn(mod, field):
     return mod._overloads.get(field, None) if isinstance(mod, ScriptModule) else None
 
 
-def _try_compile_weak_script(fn):
-    entry = _jit_internal.compiled_weak_fns.get(fn)
-    if entry is None:
-        return None
-    if entry["status"] == _jit_internal.COMPILATION_PENDING:
-        compiled_fn = torch.jit.script(fn, True, 0, entry["rcb"])
-        del entry["rcb"]
-        _jit_internal.compiled_weak_fns[fn]["compiled_fn"] = compiled_fn
-        entry["status"] = _jit_internal.COMPILED
-        return compiled_fn
-    else:
-        return entry["compiled_fn"]
-
-
 # ScriptClasses must be new-style classes because we construct them using their
 # __new__ method.
 def _is_new_style_class(cls):
